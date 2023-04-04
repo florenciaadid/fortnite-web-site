c39de2a153859cbe190a3dd446da2095b938b4b4
florenciaadid <florencia.adid@gmail.com>
2023-04-04 17:02:17 +0100

    mostrar data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import News from './News';
+import image from './assets/venom.png'
 
 function App() {
   return (
     <div className="App">
- <News/>
+      <News/>
     </div>
   );
 }
--- src/News.js
+++ src/News.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 
 function News() {
   const [data, setData] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     fetch('https://fortnite-api.com/v2/news')
@@ -10,14 +11,26 @@ function News() {
       .then(json => setData(json.data.br.motds));
   }, []);
 
+  const handlePrevious = () => {
+    setCurrentSlide((currentSlide - 1 + data.length) % data.length);
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((currentSlide + 1) % data.length);
+  };
+
   return (
     <div>
-      <h2>NEWS</h2>
-      <ul>
-        {data.map(motd => (
-          <li key={motd.id}>{motd.title}</li>
-        ))}
-      </ul>
+      <h1>Fortnite News</h1>
+      {data.length > 0 && (
+        <div>
+          <button onClick={handlePrevious}>Previous</button>
+          <button onClick={handleNext}>Next</button>
+          <img src={data[currentSlide].image} alt={data[currentSlide].title} />
+          <p>{data[currentSlide].title}</p>
+          <p>{data[currentSlide].body}</p>
+        </div>
+      )}
     </div>
   );
 }
