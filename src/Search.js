6f4ead34b30213e5a197db65c8980c8d8ea19e16
florenciaadid <florencia.adid@gmail.com>
2023-04-14 12:57:45 +0100

    search working

--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,77 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import noImage from './assets/noImage.png'
 
 function Search() {
-    const apiKey = 'eb545ec5-2071-4b4a-bac8-eed810daf620';
-    const apiUrl = 'https://fortnite-api.com/v2/stats/br/v2/78c16b18a7e04e4380546845dd0e87df';
-  
-    const [playerStats, setPlayerStats] = useState([]);
-  
-    useEffect(() => {
-      fetch(apiUrl, {
+  const apiKey = "eb545ec5-2071-4b4a-bac8-eed810daf620";
+  const apiUrl = "https://fortnite-api.com/v2/stats/br/v2";
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [playerStats, setPlayerStats] = useState(null);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`${apiUrl}/${searchTerm}`, {
         headers: {
-          Authorization: apiKey
-        }
-      })
-        .then(response => response.json())
-        .then(data => setPlayerStats(data.data.account))
-        .catch(error => console.error(error));
-    }, []);
-  
-    return (
-      <div>
-        {playerStats && playerStats.name && (
-          <>
-            <p>Player Name: {playerStats.name}</p>
-            <p>Player ID: {playerStats.id}</p>
-          </>
-        )}
-      </div>
-    );
-  }
-  
+          Authorization: apiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Player not found");
+      }
+
+      const data = await response.json();
+
+      setPlayerStats(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Copy button
+  const [copySuccess, setCopySuccess] = useState('');
+  const textToCopy = '78c16b18a7e04e4380546845dd0e87df';
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(textToCopy);
+    setCopySuccess('Copied!');
+  };
+
+  return (
+    <div>
+        <div>
+          <h2>ID's examples</h2>
+                <h2>{textToCopy}  <button onClick={handleCopy}>Copy</button></h2>
+                {copySuccess}
+         </div>
+      <form onSubmit={handleSearch}>
+        <label>
+          Player Name:
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+
+      {playerStats && (
+        <>
+          <p>Player Name: {playerStats.account.name}</p>
+          <p>Player ID: {playerStats.account.id}</p>
+          <p>Battle Pass Level: {playerStats.battlePass.level}</p>             
+          <p>Battle Pass Progress: {playerStats.battlePass.progress}</p>
+            <img src={playerStats.image || noImage } alt={playerStats.account.name} />
+            {playerStats.stats.all.overall && Object.entries(playerStats.stats.all.overall).map(([key, value]) => (
+                        <p key={key}>{key}: {value}</p>
+                    ))}
+        </>
+      )}
+    </div>
+  );
+}
 
 export default Search;
